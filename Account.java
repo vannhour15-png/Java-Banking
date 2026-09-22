744fd67a7f549b416ffb31e98e7a3b1505610f14
Your Name <choyousakphea@gmail.com>
2026-09-22 18:38:30 +0700

    Add account transaction and security methods

--- Account.java
+++ Account.java
@@ -27,4 +27,46 @@ public abstract class Account {
     public String getAccId() {
         return accId;
     }
+public void deposit(double amount) {
+    this.balance += amount;
+}
+
+public abstract void withdraw(double amount)
+        throws InsufficientFundsException;
+
+public void transfer(Account destination, double amount)
+        throws InsufficientFundsException {
+    this.withdraw(amount);
+    destination.deposit(amount);
+}
+
+protected void increaseBalance(double amount) {
+    this.balance += amount;
+}
+
+protected void decreaseBalance(double amount) {
+    this.balance -= amount;
+}
+
+public void incrementFailedPin() {
+    this.failedPin++;
+    if (this.failedPin >= 3) {
+        this.lockedStatus = true;
+    }
+}
+
+public void resetFailedPin() {
+    this.failedPin = 0;
+}
+
+public void lockAccount() {
+    this.lockedStatus = true;
+}
+
+public void unlockAccount() {
+    this.lockedStatus = false;
+    this.failedPin = 0;
+}
+
+public abstract String getAccountType();
 }
