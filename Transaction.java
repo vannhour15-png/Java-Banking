21b1b61d56325f09ae4a274b3e7076df5cb691fc
Van Hour <vannhour15@gmail.com>
2026-09-22 15:09:04 +0700

    Added Transaction methods and created a date formatter

--- Transaction.java
+++ Transaction.java
@@ -4,7 +4,6 @@ import java.time.format.DateTimeFormatter;
 public class Transaction {
 
     private static int counter = 1;
-
     private String transactionId;
     private String transactionType;
     private double amount;
@@ -16,5 +15,28 @@ public class Transaction {
     public String getAccId() {
         return accId;
     }
+    
+    public Transaction(String transactionType, double amount, String accId) {
+        this.transactionId = "T" + String.format("%03d", counter);
+        this.receiptNo = "R" + String.format("%03d", counter);
+        counter++;
 
+        this.transactionType = transactionType;
+        this.amount = amount;
+        this.accId = accId;
+
+        LocalDateTime now = LocalDateTime.now();
+        this.date = now.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+        this.time = now.format(DateTimeFormatter.ofPattern("HH:mm"));
+    }
+
+    public void viewTransaction() {
+        System.out.println("Transaction ID: " + transactionId);
+        System.out.println("Type: " + transactionType);
+        System.out.println("Amount: $" + String.format("%.2f", amount));
+        System.out.println("Date: " + date);
+        System.out.println("Time: " + time);
+        System.out.println("Receipt No: " + receiptNo);
+        System.out.println("---------------------------");
+    }
 }
