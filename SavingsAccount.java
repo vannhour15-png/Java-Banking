3cc265d1035d777bd217336720f252143aaa27fd
Your Name <choyousakphea@gmail.com>
2026-09-22 18:49:57 +0700

    Create SavingsAccount with withdrawal rule

---
+++ SavingsAccount.java
@@ -0,0 +1,29 @@
+public class SavingsAccount extends Account {
+
+    private double minimumBalance;
+    private double interestRate;
+
+    public SavingsAccount(String accId, double balance,
+                          double minimumBalance, double interestRate) {
+        super(accId, balance);
+        this.minimumBalance = minimumBalance;
+        this.interestRate = interestRate;
+    }
+
+    @Override
+    public void withdraw(double amount) throws InsufficientFundsException {
+        if (checkBalance() - amount < minimumBalance) {
+            throw new InsufficientFundsException(
+                "Withdrawal denied: balance cannot fall below the minimum balance of $"
+                + String.format("%.2f", minimumBalance) + "."
+            );
+        }
+
+        decreaseBalance(amount);
+    }
+
+    @Override
+    public String getAccountType() {
+        return "Savings";
+    }
+}
