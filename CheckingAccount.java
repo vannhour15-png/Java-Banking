800f291c39acce1ef57efe8f6c75d81e4f153126
Your Name <choyousakphea@gmail.com>
2026-09-22 18:45:42 +0700

    Add withdrawal limit validation

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -15,4 +15,25 @@ public class CheckingAccount extends Account {
     public double getWithdrawLimit() {
         return withdrawLimit;
     }
+@Override
+public void withdraw(double amount) throws InsufficientFundsException {
+    checkWithdrawLimit(amount);
+
+    if (amount > checkBalance()) {
+        throw new InsufficientFundsException("Insufficient funds.");
+    }
+
+    decreaseBalance(amount);
+}
+
+public void checkWithdrawLimit(double amount)
+        throws InsufficientFundsException {
+
+    if (amount > withdrawLimit) {
+        throw new InsufficientFundsException(
+            "Withdrawal denied: exceeds the per-transaction limit of $"
+            + String.format("%.2f", withdrawLimit) + "."
+        );
+    }
+}
 }
